f1381addceee58285d56129df34fbeaf194e9998
PIONEIRO <thyagolimamorais@gmail.com>
2026-08-29 12:10:59 -0300

    test: separate PNG validation from Godot contract

--- tests/test_visual_v2_contract.gd
+++ tests/test_visual_v2_contract.gd
@@ -1,20 +1,21 @@
 extends SceneTree
 
-const REQUIRED_ASSETS := {
-	"res://assets/art_v2/grass_tile.png": Vector2i(64, 64),
-	"res://assets/art_v2/dirt_tile.png": Vector2i(64, 64),
-	"res://assets/art_v2/tree_oak_a.png": Vector2i(160, 208),
-	"res://assets/art_v2/tree_oak_b.png": Vector2i(160, 208),
-	"res://assets/art_v2/tree_pine.png": Vector2i(160, 208),
-	"res://assets/art_v2/bush.png": Vector2i(80, 64),
-	"res://assets/art_v2/rock.png": Vector2i(80, 64),
-	"res://assets/art_v2/log.png": Vector2i(112, 64),
-	"res://assets/art_v2/flowers.png": Vector2i(64, 48),
-	"res://assets/art_v2/player_ranger_8dir.png": Vector2i(768, 128),
-	"res://assets/art_v2/monster_slime.png": Vector2i(80, 64),
-	"res://assets/art_v2/monster_boar.png": Vector2i(96, 72),
-	"res://assets/art_v2/attack_slash.png": Vector2i(96, 96)
-}
+const REQUIRED_ASSETS := [
+	"res://assets/art_v2/grass_tile.png",
+	"res://assets/art_v2/dirt_tile.png",
+	"res://assets/art_v2/tree_oak_a.png",
+	"res://assets/art_v2/tree_oak_b.png",
+	"res://assets/art_v2/tree_pine.png",
+	"res://assets/art_v2/bush.png",
+	"res://assets/art_v2/rock.png",
+	"res://assets/art_v2/log.png",
+	"res://assets/art_v2/flowers.png",
+	"res://assets/art_v2/player_ranger_8dir.png",
+	"res://assets/art_v2/monster_slime.png",
+	"res://assets/art_v2/monster_boar.png",
+	"res://assets/art_v2/attack_slash.png",
+	"res://assets/art_v2/shadow.png"
+]
 
 var failures: Array[String] = []
 
@@ -42,13 +43,8 @@ func _run() -> void:
 		_check(float(constants.get("ART_SCALE", 0.0)) >= 0.78, "Player V2 deve ocupar mais tela")
 		_check(float(constants.get("CAMERA_ZOOM", 0.0)) >= 1.55, "Camera V2 deve ser mais proxima")
 
-	for path in REQUIRED_ASSETS.keys():
+	for path in REQUIRED_ASSETS:
 		_check(FileAccess.file_exists(path), "asset V2 ausente: %s" % path)
-		if FileAccess.file_exists(path):
-			var texture := load(path) as Texture2D
-			_check(texture != null, "asset V2 deve importar como Texture2D: %s" % path)
-			if texture != null:
-				_check(texture.get_size() == REQUIRED_ASSETS[path], "dimensao inesperada em %s: %s" % [path, texture.get_size()])
 
 	if failures.is_empty():
 		print("FOREST_VISUAL_PROTOTYPE_V2_CONTRACT_PASS")
